53c77d28f6bb4984dddbc65c36cb99e95f8b98e8
Souta Sakamoto <tkintern13@gmail.com>
2014-10-03 14:56:35 +0900

    NGUIをタイトルに適用
    タイトルのボタン、背景をNGUIでの表示。

--- unity/Assets/Script/TitleScenenCtrl.cs
+++ unity/Assets/Script/TitleScenenCtrl.cs
@@ -4,25 +4,27 @@ using System.Collections;
 public class TitleScenenCtrl : MonoBehaviour {
     // タイトル画面テクスチャ
     public Texture2D bgTexture;
-
+	private bool StartButton = false;
     void OnGUI()
     {
-        // スタイルを準備.
-        GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-
         // 解像度対応
         GUI.matrix = Matrix4x4.TRS(
             Vector3.zero,
             Quaternion.identity,
             new Vector3(Screen.width / 854.0f, Screen.height / 480.0f, 1.0f));
         // タイトル画面テクスチャ表示
-        GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), bgTexture);
+        //GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), bgTexture);
 
-        // スタートボタンを作成します。
-        if (GUI.Button(new Rect(327, 290, 200, 54), "Start", buttonStyle))
+        // スタートボタンを作成します
+		if (StartButton)
         {
             // シーンの切り替えはApplication.LoadLevelを使用します。
             Application.LoadLevel("GameScene");
         }
     }
+
+	// StartButtonが押されたとき
+	void StartButtonPressed(){
+		StartButton = true;
+	}
 }
---
+++ unity/Assets/cubeScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeScript : MonoBehaviour {
+	private bool isRightButtonPressed;
+	private bool isLeftButtonPressed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isRightButtonPressed)
+		{
+			//右回転
+			this.transform.Rotate(0, 100 * Time.deltaTime, 0);
+		}
+		else if (this.isLeftButtonPressed)
+		{
+			//左回転
+			this.transform.Rotate(0, -100 * Time.deltaTime, 0);
+		}
+	}
+	/// <summary>右ボタン押された</summary>
+	void RightButtonPressed() { this.isRightButtonPressed = true; }
+	
+	/// <summary>左ボタン離れた</summary>
+	void RightButtonReleased() { this.isRightButtonPressed = false; }
+	
+	/// <summary>左ボタン押された</summary>
+	void LeftButtonPressed() { this.isLeftButtonPressed = true; }
+	
+	/// <summary>左ボタン離れた</summary>
+	void LeftButtonReleased() { this.isLeftButtonPressed = false; }
+}
