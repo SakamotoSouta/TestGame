13cc24f3a3446400cccd1d77dd2f12602580c00d
Souta Sakamoto <tkintern13@gmail.com>
2014-10-06 11:50:15 +0900

    NGUIでのクリア演出
    リザルトによってテクスチャを変えて表示する処理を実装。

--- unity/Assets/Script/GameResultGui.cs
+++ unity/Assets/Script/GameResultGui.cs
@@ -5,40 +5,46 @@ public class GameResultGui : MonoBehaviour
 {
 	GameRuleCtrl gameRuleCtrl;
 
-	float baseWidth = 854f;
-	float baseHeight = 480f;
+	float baseWidth = 800f;
+	float baseHeight = 100f;
 
 	public Texture2D gameOverTexture;
 	public Texture2D gameClearTexture;
+	public GameObject texturePrefab;
+	private GameObject parent;
+	private GameObject texture;
 	
 	void Awake()
 	{
 		gameRuleCtrl = GameObject.FindObjectOfType(typeof(GameRuleCtrl)) as GameRuleCtrl;
 	}
-	
-	void OnGUI()
-	{
+
+	void Start(){
+		parent = GameObject.Find ("Panel");
+	}
+
+	void Update(){
 		Texture2D aTexture;
 		if( gameRuleCtrl.gameClear )
 		{
 			aTexture = gameClearTexture;
+			texture = NGUITools.AddChild (parent, texturePrefab);
 		}
 		else if( gameRuleCtrl.gameOver )
 		{
 			aTexture = gameOverTexture;
+			texture = NGUITools.AddChild (parent, texturePrefab);
 		}
 		else
 		{
 			return;
 		}
-
-		// 解像度対応.
-		GUI.matrix = Matrix4x4.TRS(
-			Vector3.zero,
-			Quaternion.identity,
-			new Vector3(Screen.width / baseWidth, Screen.height / baseHeight, 1f));
-		
-		// リザルト.
-		GUI.DrawTexture(new Rect(0.0f, 208.0f, 854.0f, 64.0f), aTexture);
+		if (texture) {
+			texture.transform.localScale = new Vector3 (baseWidth, baseHeight, 0f);
+			UITexture tex = texture.GetComponent ("UITexture") as UITexture;
+			tex.mainTexture = aTexture;
+		}
 	}
+
+
 }
