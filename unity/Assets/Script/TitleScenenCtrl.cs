f9dec9800acb83b7da5504bb6dac3079271f874c
Souta Sakamoto <tkintern13@gmail.com>
2014-10-06 13:18:12 +0900

    タイトルスクリプトのコメント削除
    いらなくなったので削除

--- unity/Assets/Script/TitleScenenCtrl.cs
+++ unity/Assets/Script/TitleScenenCtrl.cs
@@ -5,21 +5,14 @@ public class TitleScenenCtrl : MonoBehaviour {
     // タイトル画面テクスチャ
     public Texture2D bgTexture;
 	private bool StartButton = false;
+
     void OnGUI()
     {
-        // 解像度対応
-        GUI.matrix = Matrix4x4.TRS(
-            Vector3.zero,
-            Quaternion.identity,
-            new Vector3(Screen.width / 854.0f, Screen.height / 480.0f, 1.0f));
-        // タイトル画面テクスチャ表示
-        //GUI.DrawTexture(new Rect(0.0f, 0.0f, 854.0f, 480.0f), bgTexture);
 
         // スタートボタンを作成します
 		if (StartButton)
         {
-            // シーンの切り替えはApplication.LoadLevelを使用します。
-            Application.LoadLevel("GameScene");
+			Application.LoadLevel("GameScene");
         }
     }
 
