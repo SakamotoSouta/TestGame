b21ae8343a6b1f2c95f6d1208596f99e970afa0b
Souta Sakamoto <tkintern13@gmail.com>
2014-10-06 09:35:51 +0900

    ステータスの表示
    プレハブから色＆名前を変更して設定した場所に表示する処理を実装。

--- unity/Assets/Script/CharacterStatusGui.cs
+++ unity/Assets/Script/CharacterStatusGui.cs
@@ -5,134 +5,130 @@ public class CharacterStatusGui : MonoBehaviour
 {
 	public GameObject labelPrefab;
 	public GameObject sliderPrefab;
-
+	
 	GameObject parent;
-
+	
 	// プレイヤーステータスGUI
 	GameObject playerSlider;
 	GameObject playerLabel;
 	// ターゲットステータスGUI
 	GameObject enemySlider;
 	GameObject enemyLabel;
-
-	// 画面の中心
-    float baseWidth = Screen.width / 2;
-    float baseHeight = Screen.height / 2;
-
-    // ステータス.
-    CharacterStatus playerStatus;
-    Vector2 playerStatusOffset = new Vector2(8f, 80f);
 	
-    // ライフバー.
-    float frontLifeBarOffsetX = 2f;
-    float lifeBarTextureWidth = 128f;
-    Color playerFrontLifeBarColor = Color.green;
-    Color enemyFrontLifeBarColor = Color.red;
-
+	// 画面の中心
+	float baseWidth = Screen.width / 2;
+	float baseHeight = Screen.height / 2;
+	
+	// ステータス.
+	CharacterStatus playerStatus;
+	Vector2 playerStatusOffset = new Vector2(-140f, 0f);
+	Vector2 enemyStatusOffset = new Vector2(-128f / 2f, -50f);	
+	// ライフバー.
+	float lifeBarTextureWidth = 128f;
+	Color playerFrontLifeBarColor = Color.green;
+	Color enemyFrontLifeBarColor = Color.red;
+	
 	void Start(){
 		parent = GameObject.Find ("Panel");
-		// NGUIの生成
-		playerSlider = NGUITools.AddChild (parent, sliderPrefab);
-		playerLabel = NGUITools.AddChild (parent, labelPrefab);
-		enemySlider = NGUITools.AddChild (parent, sliderPrefab);
-		enemyLabel = NGUITools.AddChild (parent, labelPrefab);
 	}
-
-    // プレイヤーステータスの描画.
-    void DrawPlayerStatus()
-    {
+	
+	// プレイヤーステータスの描画.
+	void DrawPlayerStatus()
+	{
 		float x = baseWidth - playerStatusOffset.x;
 		float y = playerStatusOffset.y;
-        DrawCharacterStatus(
-            x, y,
-            playerStatus,
-            playerFrontLifeBarColor,
+		if (!playerLabel) {
+			playerLabel = NGUITools.AddChild (parent, labelPrefab);
+		}
+		if (!playerSlider) {
+			playerSlider = NGUITools.AddChild (parent, sliderPrefab);
+		}
+		playerLabel.transform.localScale = new Vector2 (28f, 28f);
+		
+		UIAnchor posSlider = playerSlider.GetComponent("UIAnchor") as UIAnchor;
+		UIAnchor posLabel = playerLabel.GetComponent("UIAnchor") as UIAnchor;
+		posSlider.side = UIAnchor.Side.Right;
+		posSlider.pixelOffset = playerStatusOffset;
+		posLabel.side = UIAnchor.Side.Right;
+		posLabel.pixelOffset = playerStatusOffset;
+		posLabel.pixelOffset.x += 30;
+		posLabel.pixelOffset.y += 30;
+		
+		DrawCharacterStatus(
+			playerStatus,
+			playerFrontLifeBarColor,
 			playerLabel,
 			playerSlider);
-    }
-
-    // 敵ステータスの描画.
-    void DrawEnemyStatus()
-    {
-		if (playerStatus.lastAttackTarget != null)
-        {
-			CharacterStatus target_status = playerStatus.lastAttackTarget.GetComponent<CharacterStatus>();
-            DrawCharacterStatus(
-                baseWidth, 0f,
+	}
+	
+	// 敵ステータスの描画.
+	void DrawEnemyStatus()
+	{
+		if (playerStatus.lastAttackTarget != null) {
+			CharacterStatus target_status = playerStatus.lastAttackTarget.GetComponent<CharacterStatus> ();
+			if (!enemySlider) {
+				enemySlider = NGUITools.AddChild (parent, sliderPrefab);
+			}
+			if (!enemyLabel) {
+				enemyLabel = NGUITools.AddChild (parent, labelPrefab);
+			}
+			enemyLabel.transform.localScale = new Vector2 (28f, 28f);
+			
+			UIAnchor posSlider = enemySlider.GetComponent ("UIAnchor") as UIAnchor;
+			UIAnchor posLabel = enemyLabel.GetComponent ("UIAnchor") as UIAnchor;
+			posSlider.side = UIAnchor.Side.Top;
+			posSlider.pixelOffset = enemyStatusOffset;
+			posLabel.side = UIAnchor.Side.Top;
+			posLabel.pixelOffset = enemyStatusOffset;
+			posLabel.pixelOffset.x -= enemyStatusOffset.x;
+			posLabel.pixelOffset.y += 30; 
+			DrawCharacterStatus (
 				target_status,
-                enemyFrontLifeBarColor,
+				enemyFrontLifeBarColor,
 				enemyLabel,
 				enemySlider);
-        }
-    }
-
-    // キャラクターステータスの描画.
-    void DrawCharacterStatus(float x, float y, CharacterStatus status, Color front_color, GameObject label, GameObject slider)
-    {
-		// ラベルの取得
-		if (label != null) {
-			Transform t = label.transform;
-			t.position = new Vector3(x, y, 0); 
+		}
+		else {
+			if (enemySlider) {
+				Destroy(enemySlider);
+			}
+			if (enemyLabel) {
+				Destroy(enemyLabel);
+			}
+		}
+	}
+	
+	// キャラクターステータスの描画.
+	void DrawCharacterStatus(CharacterStatus status, Color front_color, GameObject label, GameObject slider)
+	{
+		if (label != null) { 
 			UILabel UIlabel = label.GetComponent("UILabel") as UILabel;
 			UIlabel.text = status.characterName;
 		}
-        // 名前.
-        /*GUI.Label(
-            new Rect(x, y, nameRect.width, nameRect.height),
-			status.characterName,
-            nameLabelStyle);
-
-
+		GameObject fore = slider.transform.FindChild("Foreground").gameObject;
+		UISlicedSprite s = fore.GetComponent ("UISlicedSprite") as UISlicedSprite;
+		s.color = front_color;
 		float life_value = (float)status.HP / status.MaxHP;
-		if(backLifeBarTexture != null)
-		{
-			// 背面ライフバー.
-			y += nameRect.height;
-			GUI.DrawTexture(new Rect(x, y, bar_rect.width, bar_rect.height), backLifeBarTexture);
-		}
-
-		Slider = GameObject.Find ("Slider");
-		if (Slider) {
-			UISlider slider = Slider.GetComponent("UISlider") as UISlider;
-			//GameObject Fore = GameObject.Find("Foreground");
-			//float resize_front_bar_offset_x = frontLifeBarOffsetX * bar_rect.width / lifeBarTextureWidth;
-			//float front_bar_width = bar_rect.width - resize_front_bar_offset_x * 2;
-			//var gui_color = GUI.color;
-			//GUI.color = front_color;
-			//UISlicedSprite s= Fore.GetComponent("UISlicedSprite") as UISlicedSprite;
-			slider.sliderValue = life_value;
-			//GUI.DrawTexture(new Rect(x + resize_front_bar_offset_x, y, front_bar_width * life_value, bar_rect.height), frontLifeBarTexture);
-			//GUI.color = gui_color;			
-		}
-        // 前面ライフバー.
-		if(frontLifeBarTexture != null)
-		{
-			float resize_front_bar_offset_x = frontLifeBarOffsetX * bar_rect.width / lifeBarTextureWidth;
-			float front_bar_width = bar_rect.width - resize_front_bar_offset_x * 2;
-			var gui_color = GUI.color;
-			GUI.color = front_color;
-			GUI.DrawTexture(new Rect(x + resize_front_bar_offset_x, y, front_bar_width * life_value, bar_rect.height), frontLifeBarTexture);
-			GUI.color = gui_color;
-		}
-*/
-    }
-
-    void Awake()
-    {
-        PlayerCtrl player_ctrl = GameObject.FindObjectOfType(typeof(PlayerCtrl)) as PlayerCtrl;
-        playerStatus = player_ctrl.GetComponent<CharacterStatus>();
-    }
-
-    void OnGUI()
-    {
-        // 解像度対応.
-        GUI.matrix = Matrix4x4.TRS(
-            Vector3.zero,
-            Quaternion.identity,
-            new Vector3(Screen.width / baseWidth, Screen.height / baseHeight, 1f));
-
-        // ステータス.
-        DrawPlayerStatus();
-        DrawEnemyStatus();
-    }
+		UISlider Slider = slider.GetComponent("UISlider") as UISlider;
+		Slider.sliderValue = life_value;
+	}
+	
+	void Awake()
+	{
+		PlayerCtrl player_ctrl = GameObject.FindObjectOfType(typeof(PlayerCtrl)) as PlayerCtrl;
+		playerStatus = player_ctrl.GetComponent<CharacterStatus>();
+	}
+	
+	void OnGUI()
+	{
+		// 解像度対応.
+		GUI.matrix = Matrix4x4.TRS(
+			Vector3.zero,
+			Quaternion.identity,
+			new Vector3(Screen.width / baseWidth, Screen.height / baseHeight, 1f));
+		
+		// ステータス.
+		DrawPlayerStatus();
+		DrawEnemyStatus();
+	}
 }
